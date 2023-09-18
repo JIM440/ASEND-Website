03cf477b12282d1aadf8de21f9f47d4d42fdd4c9
JIM440 <118163334+JIM440@users.noreply.github.com>
2023-09-18 21:30:37 +0100

    initial commit

---
+++ js/index.js
@@ -0,0 +1,6 @@
+const close = document.querySelector('.bx-x');
+const mobileNav = document.querySelector('.mobile-nav-menu')
+
+close.addEventListener('click', ()=>{
+mobileNav.classList.add('active')
+})
