733341c2f08815c258fe6286425a31635513df0a
JIM440 <takemjim43@gmail.com>
2023-09-21 16:48:37 +0100

    'slider_added'

--- js/index.js
+++ js/index.js
@@ -85,3 +85,21 @@ backTop.classList.add('active')
         backTop.classList.remove('active')
       }
     })
+
+    // swiper
+    var swiper = new Swiper(".mySwiper", {
+      spaceBetween: 30,
+      centeredSlides: true,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
