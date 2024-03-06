8dd7b169374c86eb10c5bcb2f5af6974d12abe51
JIM440 <takemjim43@gmail.com>
2024-03-06 20:34:22 +0100

    more changes

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -15,7 +15,7 @@ const Home = () => {
   const scrollToTop = UseScrollTop();
   return (
     <>
-      <section className="hero">
+      <section className="hero hero-home">
         <div
           className="container"
           style={{
--- src/pages/supportus/index.jsx
+++ src/pages/supportus/index.jsx
@@ -22,21 +22,58 @@ const Support = () => {
       <section className="support-bg">
         <div className="container bg-blue ">
           <h2 className="header">Support</h2>
+          {/* include here the be an impact text, say here how they can help someone by paying tuition or sponsor an activity e.g. world engineering day */}
+          <h3>Support Us</h3>
+          <br />
+
           <p>
-            {/* include here the be an impact text, say here how they can help someone by paying tuition or sponsor an activity e.g. world engineering day */}
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
-            totam in voluptatem corporis earum odit nobis, suscipit quod sed
-            dignissimos illo voluptates nam error odio velit quibusdam facilis.
-            Voluptas quidem optio, tenetur quia molestiae illo beatae, officiis
-            aspernatur, commodi quae dignissimos veniam? Tempora id a, quisquam
-            amet rerum minima iusto ab explicabo corporis esse optio
-            perspiciatis at alias accusamus fugiat possimus nostrum modi. A est
-            maiores impedit reiciendis obcaecati quaerat consequatur,
-            exercitationem inventore corrupti molestiae quasi at quae, sequi,
-            nemo error laboriosam qui fugiat voluptates quos odit distinctio
-            tempore? Quasi soluta quos amet perspiciatis voluptas error dolorum
-            fugiat asperiores exercitationem.
+            At ASEND, we are on a mission to empower Cameroonians by preparing
+            them for entrance exams into professional schools and shaping the
+            next generation of leaders.
           </p>
+          <br />
+          <br />
+
+          <h3>Our Impact:</h3>
+          <br />
+          <p>
+            With a proud legacy of success, ASEND has helped over 4000 students
+            secure admission into prestigious professional schools in Cameroon.
+            Our commitment to excellence and individualized support has
+            transformed countless lives and opened doors to brighter futures.
+          </p>
+          <br />
+          <br />
+
+          <h3>How You Can Support:</h3>
+          <br />
+          <p>
+            - <span className="bold">Pay It Forward:</span> Your contribution
+            can be the key to unlocking a world of opportunities for aspiring
+            students. By supporting us, you are investing in providing access to
+            quality education, guidance, and the possibility for students to
+            achieve their dreams of studying in their ideal school.
+          </p>
+          <br />
+          <br />
+
+          <p>
+            - <span className="bold">Sponsor an Activity:</span> Through
+            sponsoring events like World Engineering Day or funding special
+            programs, you are directly impacting the educational journey of
+            students and shaping their future in profound ways.
+          </p>
+          <br />
+          <br />
+
+          <p>
+            Join us in our commitment to nurturing talent, fostering growth, and
+            creating a brighter tomorrow for the leaders of Cameroon. Together,
+            we can make a difference that resonates far beyond the classroom
+            walls. Support us using one of the methods below!
+          </p>
+          <br />
+          <br />
         </div>
       </section>
       <section className="mw donate-options">
